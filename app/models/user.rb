8e7e0eb7271c0144447fd6c62cef6bb295dcc9ca
Nick Knight <nicholas.b.knight@gmail.com>
2018-07-19 11:34:57 -0400

    Authentication and initial MC's set

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+    has_many :tweets, dependent: :destroy
+
+    mount_uploader :avatar, AvatarUploader
 end
+
+
